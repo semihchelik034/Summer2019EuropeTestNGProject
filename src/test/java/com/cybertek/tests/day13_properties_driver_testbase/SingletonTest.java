d1c0be456ddf6179d755259506cdb1cd84f3e5a0
semih <semihchelik34@gmail.com>
2020-02-03 16:19:50 +0000

    day 13 driver,testbase,properties file

--- src/test/java/com/cybertek/tests/day13_properties_driver_testbase/SingletonTest.java
+++ src/test/java/com/cybertek/tests/day13_properties_driver_testbase/SingletonTest.java
@@ -7,15 +7,16 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 public class SingletonTest {
     @Test
-    public void test(){
+    public void test() {
         String s1 = Singleton.getInstance();
         //100 lines of code
         String s2 = Singleton.getInstance();
         System.out.println("s1 = " + s1);
         System.out.println("s2 = " + s2);
     }
+
     @Test
-    public void test2(){
+    public void test2() {
         //this is how we create webdriver object from now on
         //type of the driver will come from configuration.properties file
         WebDriver driver = Driver.get();
@@ -23,6 +24,7 @@ public class SingletonTest {
         Driver.get().get(ConfigurationReader.get("url"));
         Driver.get().navigate().refresh();
     }
+
     @Test
     public void test3() throws InterruptedException {
         Thread.sleep(2000);
@@ -30,3 +32,4 @@ public class SingletonTest {
         //if we are using Driver close, we use closeDriver method to close the driver.
         Driver.closeDriver();
     }
+}
